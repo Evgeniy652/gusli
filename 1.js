34e6b8ec27a6aa54e4b156e0b3c6efa6f5896f6c
Evgeniy652 <evgen652@mail.ru>
2021-11-10 23:26:58 +0300

    start

---
+++ 1.js
@@ -0,0 +1,37 @@
+
+  
+  // View a list of images.
+  // Note: All images within the container will be found by calling `element.querySelectorAll('img')`.
+  const gallery = new Viewer(document.getElementById('images'));
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
