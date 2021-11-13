df636b221fe3fb284d18c96f35a5093916e8d6e8
Evgeniy652 <evgen652@mail.ru>
2021-11-13 16:09:06 +0300

    flex

--- main.js
+++ main.js
@@ -4,11 +4,11 @@
 // Note: All images within the container will be found by calling `element.querySelectorAll('img')`.
 
 const globalConfig = {
-	movable: false,
+	movable: true,
 };
 
 function getGalleries() {
-	const listOfGalleries = document.querySelectorAll('#images');
+	const listOfGalleries = document.querySelectorAll('.images');
 
 	[...listOfGalleries].forEach((ul) => {
 		new Viewer(ul, globalConfig);
@@ -44,17 +44,14 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  
 }
 
 
