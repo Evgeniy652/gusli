b9ee5713a1d83a773ca18fec658328586d1e4d32
Evgeniy652 <evgen652@mail.ru>
2023-03-16 01:07:07 +0300

    fix slider

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+const slideshowContainer = document.querySelector(".slideshow-container");
+let countAllSlides = 32;
+let currentSlideNumber = 1;
+slideshowContainer.insertAdjacentHTML("afterbegin", `
+<div class="mySlides fade">
+<img class="foto" src="images/myslides/${currentSlideNumber}.jpg" alt="фото-гусли-${currentSlideNumber}">
+<div class="text"></div>
+</div>
+`);
+function nextSlides(n) {
+  currentSlideNumber += n;
+  if(currentSlideNumber === 0) {
+    currentSlideNumber= countAllSlides;
+  }
+  if(currentSlideNumber>countAllSlides){
+    currentSlideNumber= 1;
+  }
+  const mySlides = document.querySelector('.mySlides');
+  mySlides.innerHTML = `
+  <img class="foto" src="images/myslides/${currentSlideNumber}.jpg" alt="фото-гусли-${currentSlideNumber}">
+  <div class="text"></div>
+`
+}
 
   
 // View a list of images.
@@ -28,31 +51,6 @@ getGalleries();
 hideUnusedPhotos();
 
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  
-  slides[slideIndex-1].style.display = "block";
-  
-}
 
 
 
