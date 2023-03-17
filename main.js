bc2a841813f77352c15a467214b811af1b904328
Aliaksandr Kakhanovich <aliaksandr_kakhanovich@epam.com>
2023-03-17 23:12:03 +0300

    feat: optimaze imgs and add lazy loading for slider

--- main.js
+++ main.js
@@ -1,30 +1,73 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// Показывает стартовое значение для слайдера
+let CURRENT_SLIDER_NUMBER = 1;
+// Показывает текущее значение для слайдера
+const START_SLIDER_NUMBER = 1;
+// Показывает кол-во фото для слайдера
+const NUMBER_OF_PHOTOS_FOR_SLIDER = 28;
+const sliderContainer = document.querySelector('.slider-container');
+
+window.onload = () => {
+  let countSlides = 0;
+  const slides = document.querySelectorAll('.my-slide');
+
+  countSlides = slides.length;
+
+  for (let index = countSlides + 1; index < NUMBER_OF_PHOTOS_FOR_SLIDER + 1; index++) {
+    loadSlide(index, 'hide-slide');
+  }
+}
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+function rightSlide() {
+  if (CURRENT_SLIDER_NUMBER === NUMBER_OF_PHOTOS_FOR_SLIDER) {
+    return;
   }
 
-  slides[slideIndex-1].style.display = "block";
-
+  const prevNumber = CURRENT_SLIDER_NUMBER;
+  CURRENT_SLIDER_NUMBER++;
+  showSlide(prevNumber);
 }
 
-  
+function leftSlide() {
+  if (CURRENT_SLIDER_NUMBER === START_SLIDER_NUMBER) {
+    return;
+  }
+
+  const prevNumber = CURRENT_SLIDER_NUMBER;
+  CURRENT_SLIDER_NUMBER--;
+  showSlide(prevNumber);
+}
+
+function showSlide(currentNumber) {
+  const currentSlide = document.querySelector(`[data-id="slide-${currentNumber}"]`);
+  const nextSlide = document.querySelector(`[data-id="slide-${CURRENT_SLIDER_NUMBER}"]`)
+
+  if (!currentSlide) {
+    console.log('There is not current slide, check please you functionality');
+    return;
+  }
+
+  // Скрываем текущий слайд (фото)
+  currentSlide.classList.add('hide-slide');
+
+  if (nextSlide) {
+    nextSlide.classList.remove('hide-slide');
+    return;
+  }
+
+  loadSlide(CURRENT_SLIDER_NUMBER, '');
+}
+
+function loadSlide(number, hide) {
+  const newSlide = `
+  <div class="fade ${hide}" data-id="slide-${number}">
+    <img class="foto" src="images/myslides/${number}.jpg" alt="фото-гусли-${number}">
+    <div class="text"></div>
+  </div>`;
+
+  sliderContainer.insertAdjacentHTML('beforeend', newSlide);
+}
+
 // View a list of images.
 // Note: All images within the container will be found by calling `element.querySelectorAll('img')`.
 
